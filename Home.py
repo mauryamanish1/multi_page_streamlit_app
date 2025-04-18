3756af524c5a4c8abd8f246ea96ee1e910c1845e
mauryamanish1 <manish.maurya.ai@gmail.com>
2025-04-18 13:13:38 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Welcome to the Language Tools Interface")
 st.write("Navigate using the sidebar to access different tools.")
-st.write("The translation search is  currently built for documents belonging to 'ZS115670_', '47797881_ series' ")
+# st.write("The translation search is  currently built for documents belonging to 'ZS115670_', '47797881_ series' ")
