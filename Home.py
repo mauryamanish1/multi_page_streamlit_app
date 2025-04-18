7f4d36534174c042b7beef1f10f2c2256853b8df
mauryamanish1 <manish.maurya.ai@gmail.com>
2025-04-18 12:14:12 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("Welcome to the Language Tools App")
+st.title("Welcome to the Language Tools Interface")
 st.write("Navigate using the sidebar to access different tools.")
+st.write("The translation search is  currently built for documents belonging to 'ZS115670_', '47797881_ series' ")
